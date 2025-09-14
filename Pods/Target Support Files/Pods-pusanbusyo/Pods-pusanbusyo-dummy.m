40f34f5369eb4016594456b349de05d2f0010d81
djk <joojaepapa@gmail.com>
2025-09-14 19:33:53 +0900

    크래시 해결 및 사라짐 완화

---
+++ Pods/Target Support Files/Pods-pusanbusyo/Pods-pusanbusyo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pusanbusyo : NSObject
+@end
+@implementation PodsDummy_Pods_pusanbusyo
+@end
---
+++ pusanbusyo/pusanbusyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pusanbusyoApp.swift
+//  pusanbusyo
+//
+//  Created by 김동준 on 9/13/25.
+//
+
+import SwiftUI
+//
+//@main
+//struct pusanbusyoApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
